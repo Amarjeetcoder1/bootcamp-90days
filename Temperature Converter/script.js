344a4146ff065f3738e1b7dbfab38112facd7248
Amarjeet Ghosh <amarjeetrajking@gmail.com>
2025-07-19 12:47:33 +0530

    Add Temperature Converter Assignment

---
+++ Temperature Converter/script.js
@@ -0,0 +1,14 @@
+//Temperature Convert: Celsius to Fahrenheit
+
+let celsius = 45;
+let fahrenheit = (celsius * 9 / 5) + 32;
+
+console.log(celsius + "°C is equal to " + fahrenheit + "°F");
+
+// Reassign the value 
+celsius = 100;
+fahrenheit = (celsius * 9 / 5) + 32;
+
+console.log(celsius + "°C is equal to " + fahrenheit + "°F");
+
+
